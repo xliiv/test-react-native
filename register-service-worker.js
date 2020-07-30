c5ddc98c2e2ee344ccdd52a4f6d682afcc3d19e5
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2020-07-30 10:50:05 +0200

    Updates

---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/expo-gh-pages/expo-service-worker.js",{scope:"/expo-gh-pages/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
